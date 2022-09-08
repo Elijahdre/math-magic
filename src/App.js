50fd456b8f19036b33612810989797928e59558e
Elijah Odjegba <elijahodjegba@gmail.com>
2022-09-08 12:50:00 +0100

    Build app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-function App() {
-	return (
-		null
-	);
+/* eslint-disable */
+
+import React from 'react';
+import Calculator from './components/Calculator';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
